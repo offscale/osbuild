d9c450cd07260a2b9560470e952471909a0aadf2
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-04-30 18:34:27 +1000

    [docopt] .docopt,src/docopt.c

---
+++ src/docopt.c
@@ -0,0 +1,338 @@
+#ifdef __cplusplus
+#include <cstdio>
+#include <cstdlib>
+#include <cstddef>
+#include <cstring>
+#else
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <string.h>
+#endif
+
+
+typedef struct {
+    /* options without arguments */
+    size_t check;
+    size_t distribution;
+    size_t help;
+    size_t no_cache;
+    size_t no_check;
+    size_t no_update;
+    size_t version;
+    /* special */
+    const char *usage_pattern;
+    const char *help_message;
+} DocoptArgs;
+
+const char help_message[] =
+"osbuild: installs compiler and build tools for your platform.\n"
+"E.g., on Ubuntu it will run `apt-get install -y build-essential.`\n"
+"\n"
+"Usage:\n"
+"  osbuild --check\n"
+"  osbuild --no-check\n"
+"  osbuild --no-cache\n"
+"  osbuild --no-update\n"
+"  osbuild --distribution\n"
+"  osbuild --help\n"
+"  osbuild --version\n"
+"\n"
+"Options:\n"
+"  -h --help       Show this screen.\n"
+"  --version       Show version.\n"
+"  --check         Only check if installed, don't install anything.\n"
+"  --no-check      Optimisation argument, set if you know that nothing is installed.\n"
+"  --no-cache      Skip cache. Known to work with `apk` (Alpine Linux).\n"
+"  --no-update     Skip update. E.g., `apk update`, `apt-get update`.\n"
+"  --distribution  Operating System distribution. E.g., 'OpenIndiana', 'alpine', 'macOS'. Will derive when unspecified.\n"
+"";
+
+const char usage_pattern[] =
+"Usage:\n"
+"  osbuild --check\n"
+"  osbuild --no-check\n"
+"  osbuild --no-cache\n"
+"  osbuild --no-update\n"
+"  osbuild --distribution\n"
+"  osbuild --help\n"
+"  osbuild --version";
+
+typedef struct {
+    const char *name;
+    bool value;
+} Command;
+
+typedef struct {
+    const char *name;
+    char *value;
+    char **array;
+} Argument;
+
+typedef struct {
+    const char *oshort;
+    const char *olong;
+    bool argcount;
+    bool value;
+    char *argument;
+} Option;
+
+typedef struct {
+    size_t n_commands;
+    size_t n_arguments;
+    size_t n_options;
+    Command *commands;
+    Argument *arguments;
+    Option *options;
+} Elements;
+
+
+/*
+ * Tokens object
+ */
+
+typedef struct Tokens {
+    size_t argc;
+    char **argv;
+    size_t i;
+    char *current;
+} Tokens;
+
+Tokens tokens_new(size_t argc, char **argv) {
+    Tokens ts = {argc, argv, 0, argv[0]};
+    return ts;
+}
+
+Tokens* tokens_move(Tokens *ts) {
+    if (ts->i < ts->argc) {
+        ts->current = ts->argv[++ts->i];
+    }
+    if (ts->i == ts->argc) {
+        ts->current = NULL;
+    }
+    return ts;
+}
+
+
+/*
+ * ARGV parsing functions
+ */
+
+size_t parse_doubledash(Tokens *ts, Elements *elements) {
+    //size_t n_commands = elements->n_commands;
+    //size_t n_arguments = elements->n_arguments;
+    //Command *commands = elements->commands;
+    //Argument *arguments = elements->arguments;
+
+    // not implemented yet
+    // return parsed + [Argument(None, v) for v in tokens]
+    return 0;
+}
+
+size_t parse_long(Tokens *ts, Elements *elements) {
+    size_t i;
+    size_t len_prefix;
+    size_t n_options = elements->n_options;
+    char *eq = strchr(ts->current, '=');
+    Option *option;
+    Option *options = elements->options;
+
+    len_prefix = (eq-(ts->current))/sizeof(char);
+    for (i=0; i < n_options; i++) {
+        option = &options[i];
+        if (!strncmp(ts->current, option->olong, len_prefix))
+            break;
+    }
+    if (i == n_options) {
+        // TODO '%s is not a unique prefix
+        fprintf(stderr, "%s is not recognized\n", ts->current);
+        return 1;
+    }
+    tokens_move(ts);
+    if (option->argcount) {
+        if (eq == NULL) {
+            if (ts->current == NULL) {
+                fprintf(stderr, "%s requires argument\n", option->olong);
+                return 1;
+            }
+            option->argument = ts->current;
+            tokens_move(ts);
+        } else {
+            option->argument = eq + 1;
+        }
+    } else {
+        if (eq != NULL) {
+            fprintf(stderr, "%s must not have an argument\n", option->olong);
+            return 1;
+        }
+        option->value = true;
+    }
+    return 0;
+}
+
+size_t parse_shorts(Tokens *ts, Elements *elements) {
+    char *raw;
+    size_t i;
+    size_t n_options = elements->n_options;
+    Option *option;
+    Option *options = elements->options;
+
+    raw = &ts->current[1];
+    tokens_move(ts);
+    while (raw[0] != '\0') {
+        for (i=0; i < n_options; i++) {
+            option = &options[i];
+            if (option->oshort != NULL && option->oshort[1] == raw[0])
+                break;
+        }
+        if (i == n_options) {
+            // TODO -%s is specified ambiguously %d times
+            fprintf(stderr, "-%c is not recognized\n", raw[0]);
+            return 1;
+        }
+        raw++;
+        if (!option->argcount) {
+            option->value = true;
+        } else {
+            if (raw[0] == '\0') {
+                if (ts->current == NULL) {
+                    fprintf(stderr, "%s requires argument\n", option->oshort);
+                    return 1;
+                }
+                raw = ts->current;
+                tokens_move(ts);
+            }
+            option->argument = raw;
+            break;
+        }
+    }
+    return 0;
+}
+
+size_t parse_argcmd(Tokens *ts, Elements *elements) {
+    size_t i;
+    size_t n_commands = elements->n_commands;
+    //size_t n_arguments = elements->n_arguments;
+    Command *command;
+    Command *commands = elements->commands;
+    //Argument *arguments = elements->arguments;
+
+    for (i=0; i < n_commands; i++) {
+        command = &commands[i];
+        if (!strcmp(command->name, ts->current)){
+            command->value = true;
+            tokens_move(ts);
+            return 0;
+        }
+    }
+    // not implemented yet, just skip for now
+    // parsed.append(Argument(None, tokens.move()))
+    /*fprintf(stderr, "! argument '%s' has been ignored\n", ts->current);
+    fprintf(stderr, "  '");
+    for (i=0; i<ts->argc ; i++)
+        fprintf(stderr, "%s ", ts->argv[i]);
+    fprintf(stderr, "'\n");*/
+    tokens_move(ts);
+    return 0;
+}
+
+size_t parse_args(Tokens *ts, Elements *elements) {
+    size_t ret;
+
+    while (ts->current != NULL) {
+        if (strcmp(ts->current, "--") == 0) {
+            ret = parse_doubledash(ts, elements);
+            if (!ret) break;
+        } else if (ts->current[0] == '-' && ts->current[1] == '-') {
+            ret = parse_long(ts, elements);
+        } else if (ts->current[0] == '-' && ts->current[1] != '\0') {
+            ret = parse_shorts(ts, elements);
+        } else
+            ret = parse_argcmd(ts, elements);
+        if (ret) return ret;
+    }
+    return 0;
+}
+
+size_t elems_to_args(Elements *elements, DocoptArgs *args, bool help,
+                  const char *version){
+    Command *command;
+    Argument *argument;
+    Option *option;
+    size_t i;
+
+    // fix gcc-related compiler warnings (unused)
+    (void)command;
+    (void)argument;
+
+    /* options */
+    for (i=0; i < elements->n_options; i++) {
+        option = &elements->options[i];
+        if (help && option->value && !strcmp(option->olong, "--help")) {
+            printf("%s", args->help_message);
+            return 1;
+        } else if (version && option->value &&
+                   !strcmp(option->olong, "--version")) {
+            printf("%s\n", version);
+            return 1;
+        } else if (!strcmp(option->olong, "--check")) {
+            args->check = option->value;
+        } else if (!strcmp(option->olong, "--distribution")) {
+            args->distribution = option->value;
+        } else if (!strcmp(option->olong, "--help")) {
+            args->help = option->value;
+        } else if (!strcmp(option->olong, "--no-cache")) {
+            args->no_cache = option->value;
+        } else if (!strcmp(option->olong, "--no-check")) {
+            args->no_check = option->value;
+        } else if (!strcmp(option->olong, "--no-update")) {
+            args->no_update = option->value;
+        } else if (!strcmp(option->olong, "--version")) {
+            args->version = option->value;
+        }
+    }
+    /* commands */
+    for (i=0; i < elements->n_commands; i++) {
+        command = &elements->commands[i];
+    }
+    /* arguments */
+    for (i=0; i < elements->n_arguments; i++) {
+        argument = &elements->arguments[i];
+    }
+    return 0;
+}
+
+
+/*
+ * Main docopt function
+ */
+
+DocoptArgs docopt(size_t argc, char *argv[], bool help, const char *version) {
+    DocoptArgs args = {
+        0, 0, 0, 0, 0, 0, 0,
+        usage_pattern, help_message
+    };
+    Tokens ts;
+    Command commands[] = {
+    };
+    Argument arguments[] = {
+    };
+    Option options[] = {
+        {NULL, "--check", 0, 0, NULL},
+        {NULL, "--distribution", 0, 0, NULL},
+        {"-h", "--help", 0, 0, NULL},
+        {NULL, "--no-cache", 0, 0, NULL},
+        {NULL, "--no-check", 0, 0, NULL},
+        {NULL, "--no-update", 0, 0, NULL},
+        {NULL, "--version", 0, 0, NULL}
+    };
+    Elements elements = {0, 0, 7, commands, arguments, options};
+
+    ts = tokens_new(argc, argv);
+    if (parse_args(&ts, &elements))
+        exit(EXIT_FAILURE);
+    if (elems_to_args(&elements, &args, help, version))
+        exit(EXIT_SUCCESS);
+    return args;
+}
